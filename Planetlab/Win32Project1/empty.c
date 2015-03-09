f4ade98d565a78504b265c23978fe7dc20fd60e3
rebeckalauren <rebecka.lauren@hotmail.com>
2015-03-09 15:17:20 +0100

    dialogs done

--- Planetlab/Win32Project1/empty.c
+++ Planetlab/Win32Project1/empty.c
@@ -1,7 +1,37 @@
 #include <windows.h>
+#include "resource.h"
+#include <tchar.h>
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow ) 
 {
+	HWND hDlg;
+	MSG msg;
+	BOOL ret;
+
+	InitCommonControls();
+	hDlg = CreateDialogParam(hInst, MAKEINTRESOURCE(IDD_DIALOG1), 0, DialogProc, 0);
+	ShowWindow(hDlg, nCmdShow);
+
+  while((ret = GetMessage(&msg, 0, 0, 0)) != 0) 
+  {
+    if(ret == -1)
+      return -1;
+
+    if(!IsDialogMessage(hDlg, &msg)) 
+	{
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+
+
+
+
+
 	MessageBox(NULL, "It works man?\n", "A cool Mbox", 0);
 	return 1;
+}
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  return FALSE;
 }
